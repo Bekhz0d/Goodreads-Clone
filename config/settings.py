24692baf8dd23263a3adeb9232986c963e006a51
Bekhzod <142194456+Bekhz0d@users.noreply.github.com>
2024-02-15 07:58:42 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': 'bekhzod',
         'HOST': '0.0.0.0',
         'PORT': '5432',
-        'PASSWORD': 'Bexz0djon99+',
+        'PASSWORD': '',
 
         'TEST': {
         'NAME': 'test_db',
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bekhzodnabijonov@gmail.com'
-EMAIL_HOST_PASSWORD = 'sadm ybgz kxri xasr'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
